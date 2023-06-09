31bfbb1fa83fb44991f80264737dab58dee75d2c
Dio <a0981551262@gmail.com>
2023-06-09 15:10:10 +0800

    1. change coding style to google coding style 2. add annotations for variables

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 import random
 
-test_index = 50
-test_rounds = 10000
-p = 0.5
+
+TEST_INDEX = 50
+# How many steps the walking process has.
+TEST_ROUNDS = 10000
+# Shows the result of how many times the walking process is performed.
+P = 0.5
+# The variable that determines the probability of going forward.
 
 
 def walking():
-    global test_index
+    global TEST_INDEX
     x = 0
-    for i in range(test_index):
-        if random.random() < p:
+    for i in range(TEST_INDEX):
+        if random.random() < P:
             x -= 1
         else:
             x += 1
@@ -18,20 +22,18 @@ def walking():
 
 
 def probability_test():
-    global test_index
-    data_list = []
-    for i in range(2 * test_index + 1):
-        data_list.append(0)
-    for i in range(test_rounds):
+    global TEST_INDEX
+    data_list = [0] * (2 * TEST_INDEX + 1)
+    for _ in range(TEST_ROUNDS):
         result = walking()
-        data_list[result+test_index] += 1
+        data_list[result + TEST_INDEX] += 1
     return data_list
 
 
 if __name__ == '__main__':
     data = probability_test()
     fig = plt.figure()
-    for i in range(test_index+1):
-        plt.bar(i, data[test_index+i], width=0.4, color='red')
-        plt.bar(-i, data[test_index-i], width=0.4, color='red')
+    for i in range(TEST_INDEX + 1):
+        plt.bar(i, data[TEST_INDEX + i], width=0.4, color='red')
+        plt.bar(-i, data[TEST_INDEX - i], width=0.4, color='red')
     plt.show()
