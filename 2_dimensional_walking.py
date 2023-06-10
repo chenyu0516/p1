093e70fb694c356b91bed0c2051e1cd15dd02bb1
Dio <a0981551262@gmail.com>
2023-06-10 16:35:19 +0800

    debug finish. Fix the problem of 2-D array, data_list.

--- 2_dimensional_walking.py
+++ 2_dimensional_walking.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import random
 
 
-TEST_INDEX_x = 50
+TEST_INDEX_x = 20
 # How many steps the walking process in x-axis has.
-TEST_INDEX_y = 50
+TEST_INDEX_y = 20
 # How many steps the walking process in y-axis has.
-TEST_ROUNDS = 100
+TEST_ROUNDS = 10000
 # Shows the result of how many times the walking process is performed.
 P_x = 0.5
 # The variable that determines the probability of going forward in x-axis.
@@ -31,12 +31,14 @@ def walking(a, b):
 
 
 def probability_test(a, b):
-    data_list_y = [0] * (2 * b + 1)
-    data_list = [data_list_y] * (2 * a + 1)
-    for _ in range(TEST_ROUNDS):
+    data_list = [[0 for _ in range(2*b+1)]for _ in range(2*a+1)]
+    for i in range(TEST_ROUNDS):
         result = walking(a, b)
+        print(result)
         data_list[a+result[0]][b+result[1]] += 1
+    print(data_list)
     return data_list
+
 
 
 if __name__ == '__main__':
@@ -46,5 +48,6 @@ if __name__ == '__main__':
     for i in range(2*TEST_INDEX_x+1):
         for j in range(2*TEST_INDEX_y+1):
             if data[i][j] != 0:
-                ax.scatter(i-TEST_INDEX_x, j-TEST_INDEX_y, data[i][j])
+                ax.scatter(i-TEST_INDEX_x, j-TEST_INDEX_y, data[i][j], color='r')
     plt.show()
+
